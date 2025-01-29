cc802ae6eb1944a6d6ff59304e52b04ee08993fe
1U23IT009-ANIRUDH <139449348+1U23IT009-ANIRUDH@users.noreply.github.com>
2025-01-29 09:15:07 +0000

    Done4

--- Array_Insertion.java
+++ Array_Insertion.java
@@ -2,41 +2,41 @@ import java.util.Scanner;
 
 public class Array_Insertion {
     public static void main(String[] args) {
-            int len, p, e;
-                    System.out.println("ARRAY OPERATION");
-                            Scanner sc = new Scanner(System.in);
+        int len, p, e;
+        System.out.println("ARRAY OPERATION");
+        Scanner sc = new Scanner(System.in);
 
-                                    System.out.println("Enter the length of the Array");
-                                            len = sc.nextInt();
-                                                    int arr[] = new int[len + 1];
+        System.out.println("Enter the length of the Array");
+        len = sc.nextInt();
+        int arr[] = new int[len + 1];
 
-                                                            System.out.println("Enter " + len + " element(s) to insert");
-                                                                    for (int i = 0; i < len; i++) {
-                                                                                arr[i] = sc.nextInt();
-                                                                                        }
+        System.out.println("Enter " + len + " element(s) to insert");
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-                                                                                                System.out.println("Enter the position where you want to insert:");
-                                                                                                        p = sc.nextInt();
-                                                                                                                System.out.println("Enter the element to be inserted");
-                                                                                                                        e = sc.nextInt();
+        System.out.println("Enter the position where you want to insert:");
+        p = sc.nextInt();
+        System.out.println("Enter the element to be inserted");
+        e = sc.nextInt();
 
-                                                                                                                                arrayInsert(arr, p, e, len);
+        arrayInsert(arr, p, e, len);
 
-                                                                                                                                        sc.close();
-                                                                                                                                            }
+        sc.close();
+    }
 
-                                                                                                                                                static void arrayInsert(int arr[], int p, int e, int len) {
-                                                                                                                                                        for (int i = len - 1; i >= p - 1; i--) {
-                                                                                                                                                                    arr[i + 1] = arr[i];
-                                                                                                                                                                            }
+    static void arrayInsert(int arr[], int p, int e, int len) {
+        for (int i = len - 1; i >= p - 1; i--) {
+            arr[i + 1] = arr[i];
+        }
 
-                                                                                                                                                                                    arr[p - 1] = e;
+        arr[p - 1] = e;
 
-                                                                                                                                                                                            System.out.println("After Inserting");
-                                                                                                                                                                                                    System.out.println("Length of the array: " + (len + 1));
+        System.out.println("After Inserting");
+        System.out.println("Length of the array: " + (len + 1));
 
-                                                                                                                                                                                                            for (int i = 0; i < len + 1; i++) {
-                                                                                                                                                                                                                        System.out.println("arr[" + i + "]="+arr[i]);
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                    }
+        for (int i = 0; i < len + 1; i++) {
+            System.out.println("arr[" + i + "]="+arr[i]);
+        }
+    }
+}
